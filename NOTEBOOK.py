d6f4549f8d70baa97580f00beb0615c2ae1a44da
ashkan0201 <ashkan02011@gmial.com>
2023-09-04 14:05:42 +0330

    added class NOTE

--- NOTEBOOK.py
+++ NOTEBOOK.py
@@ -2,3 +2,11 @@
 
 # Add the desired library
 import json5
+
+# This class maintains a series of required values
+class NOTE:
+    def __init__(self):
+        self.ID = None
+        self.TITLE = None
+        self.COMMENT = None
+        self.DEMO = {"Users":[]}
